5f425ce4ca8b759dae7677105b98f71064d54b89
Daniel Sochor <danielsochor@gmail.com>
2019-04-29 06:25:28 -0500

    Separation of js files, update index to match

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,21 @@
-$(document).ready(function(){
-    $('select').formSelect();
+$(document).ready(function () {
+
+  $(document).on("click", "#submit-button", function () {
+    var fromStreet = $("#a-street").val();
+    var fromCity = $("#a-city").val();
+    var fromState = $("#a-state").val();
+    var fromZipCode = $("#a-zip").val();
+    var toStreet = $("#b-street").val();
+    var toCity = $("#b-city").val();
+    var toState = $("#b-state").val();
+    var toZipCode = $("#b-zip").val();
+    var fromAddressString = fromStreet + " " + fromCity + " " + fromState + " " + fromZipCode;
+    var toAddressString = toStreet + " " + toCity + " " + toState + " " + toZipCode;
+
+    console.log(toCity);
+    //weather at desitination is given
+    getWeather(toCity);
+    getLocation(fromAddressString, toAddressString);
   });
+
+});
